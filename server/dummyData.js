467248d4ce51c6b40b832291b05245c3e31d63e2
PetterFogel <petterfogel96@gmail.com>
2022-06-06 22:04:10 +0200

    Add some dummyData

---
+++ server/dummyData.js
@@ -0,0 +1,67 @@
+// Projects
+export const books = [
+  {
+    id: "1",
+    authorId: "1",
+    name: "Harry Potter and the Sorcerer's Stone",
+    description:
+      "An orphaned boy enrolls in a school of wizardry, where he learns the truth about himself, his family and the terrible evil that haunts the magical world.",
+    genre: "Fantasy",
+    published: "26-07-1997"
+  },
+  {
+    id: "2",
+    authorId: "1",
+    name: "Harry Potter and the Chamber of Secrets",
+    description:
+      "An ancient prophecy seems to be coming true when a mysterious presence begins stalking the corridors of a school of magic and leaving its victims paralyzed.",
+    genre: "Fantasy",
+    published: "02-06-1999"
+  },
+  {
+    id: "3",
+    clientId: "2",
+    name: "Pet Semetary",
+    description:
+      "The road in front of Dr. Louis Creed's rural Maine home frequently claims the lives of neighborhood pets. Louis has recently moved from Chicago to Ludlow with his wife Rachel, their children and pet cat. Near their house, local children have created a cemetery for the dogs and cats killed by the steady stream of transports on the busy highway. Deeper in the woods lies another graveyard, an ancient Indian burial ground whose sinister properties Louis discovers when the family cat is killed.",
+    genre: "Horror",
+    published: "14-11-1983"
+  },
+  {
+    id: "4",
+    clientId: "2",
+    name: "IT",
+    description:
+      "A promise made twenty-eight years ago calls seven adults to reunite in Derry, Maine, where as teenagers they battled an evil creature that preyed on the city's children. Unsure that their Losers Club had vanquished the creature all those years ago, the seven had vowed to return to Derry if IT should ever reappear. Now, children are being murdered again and their repressed memories of that summer return as they prepare to do battle with the monster lurking in Derry's sewers once more.",
+    genre: "Horror",
+    published: "15-09-1986"
+  },
+  {
+    id: "5",
+    clientId: "3",
+    name: "Snabba Cash",
+    description:
+      "EASY MONEY - the Swedish bestselling sensation, a dark and brutal account of the Stockholm underworld. Jorge knows one thing - he's never going back inside. Mrado knows one thing - he's not going to kill for cash anymore. JW knows nothing - and that's why he's in too deep. Their paths are about to cross, but are they on the same side?",
+    genre: "Crime",
+    published: "08-05-2006"
+  }
+];
+
+// Clients
+export const authors = [
+  {
+    id: "1",
+    name: "J.K Rowling",
+    nationality: "English"
+  },
+  {
+    id: "2",
+    name: "Stephen King",
+    nationality: "American"
+  },
+  {
+    id: "3",
+    name: "Jens Lapidus",
+    nationality: "Swedish"
+  }
+];
