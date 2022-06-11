408ead35db4378a32039c2d1173b2c336cc9a05b
PetterFogel <petterfogel96@gmail.com>
2022-06-11 15:24:45 +0200

    Implement cors

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { graphqlHTTP } from "express-graphql";
 import { schema } from "./schema/schema.js";
 import { connectDB } from "./config/db.js";
@@ -9,6 +10,8 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 connectDB();
+
+app.use(cors());
 
 app.use(
   "/graphql",
