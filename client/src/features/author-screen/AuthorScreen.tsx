61da49c0723a7079b010b42944b7e5d495a8d2a6
PetterFogel <petterfogel96@gmail.com>
2022-06-12 15:21:26 +0200

    Implement useQuery to fetch data, loading and error state and render them

--- client/src/features/author-screen/AuthorScreen.tsx
+++ client/src/features/author-screen/AuthorScreen.tsx
@@ -1,9 +1,17 @@
 import { FC } from "react";
+import { useQuery } from "@apollo/client";
+import { GET_AUTHORS } from "../../apollo/templates/author/authorQueries";
+import { AuthorList } from "./AuthorList";
 
 export const AuthorScreen: FC = () => {
+  const { data: { authors } = {}, loading, error } = useQuery(GET_AUTHORS);
+
+  if (error) return <p>Something went wrong</p>;
+
   return (
     <>
-      <h2>Author screen</h2>
+      <h2>Author Screen</h2>
+      {loading ? <p>Loading...</p> : <AuthorList authors={authors} />}
     </>
   );
 };
--- client/src/features/book-screen/BookScreen.tsx
+++ client/src/features/book-screen/BookScreen.tsx
@@ -1,9 +1,17 @@
+import { useQuery } from "@apollo/client";
 import { FC } from "react";
+import { GET_BOOKS } from "../../apollo/templates/book/bookQueries";
+import { BookList } from "./BookList";
 
 export const BookScreen: FC = () => {
+  const { data: { books } = {}, loading, error } = useQuery(GET_BOOKS);
+
+  if (error) return <p>Something went wrong</p>;
+
   return (
     <>
       <h2>Book Screen</h2>
+      {loading ? <p>Loading...</p> : <BookList books={books} />}
     </>
   );
 };
