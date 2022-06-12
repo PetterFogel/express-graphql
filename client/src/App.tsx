13d8d2c13f33a1145c97a729afc4ee374be8344f
PetterFogel <petterfogel96@gmail.com>
2022-06-12 15:20:55 +0200

    Implement apollo provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,19 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { FC } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./app-routes/AppRoutes";
 
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache()
+});
+
 export const App: FC = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ApolloProvider>
   );
 };
