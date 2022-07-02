784b2ee30e6c71c0c1b159c5c93c26220130e78d
PetterFogel <petterfogel96@gmail.com>
2022-07-02 11:40:47 +0200

    Implement navigation for home buttons

--- client/src/features/home-screen/HomeScreen.tsx
+++ client/src/features/home-screen/HomeScreen.tsx
@@ -1,14 +1,28 @@
 import { FC } from "react";
+import { useNavigate } from "react-router-dom";
+import { routeFactory } from "../../routes/routeFactory";
 import classes from "./style/homeScreen.module.css";
 
 export const HomeScreen: FC = () => {
+  const navigate = useNavigate();
+
   return (
     <div className={classes.root}>
       <div className={classes.homeInfoContainer}>
         <h1 className={classes.homeTitle}>Add a new book or author</h1>
         <h3 className={classes.homeSubtitle}>check out the lists</h3>
-        <button className={classes.primaryBtn}>Books</button>
-        <button className={classes.secondaryBtn}>Authors</button>
+        <button
+          className={classes.primaryBtn}
+          onClick={() => navigate(routeFactory.bookScreen.bookList())}
+        >
+          Books
+        </button>
+        <button
+          className={classes.secondaryBtn}
+          onClick={() => navigate(routeFactory.authorScreen.authorList())}
+        >
+          Authors
+        </button>
       </div>
       <div className={classes.circle2}></div>
       <div className={classes.circle1}></div>
