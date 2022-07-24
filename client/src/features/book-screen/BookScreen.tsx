fdaae4c1e499a1f10bccfc1ee3f1211ad675ccf9
PetterFogel <petterfogel96@gmail.com>
2022-07-24 09:17:52 +0200

    Implement data types for queries + styling

--- client/src/features/book-screen/BookScreen.tsx
+++ client/src/features/book-screen/BookScreen.tsx
@@ -2,13 +2,18 @@ import { useQuery } from "@apollo/client";
 import { FC } from "react";
 import { GET_BOOKS } from "../../apollo/templates/book/bookQueries";
 import { Loader } from "../../common/components/loader/Loader";
+import { BooksData } from "../../models/book";
 import { BookList } from "./BookList";
 import classes from "./style/bookStyle.module.css";
 
 export const BookScreen: FC = () => {
-  const { data: { books } = {}, loading, error } = useQuery(GET_BOOKS);
+  const {
+    data: { books } = {},
+    loading,
+    error,
+  } = useQuery<BooksData>(GET_BOOKS);
 
-  if (error) return <p>Something went wrong</p>;
+  if (error || !books) return <p>Something went wrong</p>;
 
   return (
     <>
--- client/src/models/book.ts
+++ client/src/models/book.ts
@@ -3,7 +3,16 @@ import { Author } from "./author";
 export interface Book {
   id: string;
   name: string;
+  description: string;
   genre: string;
   published: string;
   author: Author;
+}
+
+export interface BooksData {
+  books: Book[];
+}
+
+export interface BookData {
+  book: Book;
 }
