f4942600d50a7faba1c48a359e635567aa3f1845
PetterFogel <petterfogel96@gmail.com>
2022-07-25 18:52:01 +0200

    Removed promise type

--- client/src/features/book-screen/BookScreen.tsx
+++ client/src/features/book-screen/BookScreen.tsx
@@ -2,18 +2,13 @@ import { useQuery } from "@apollo/client";
 import { FC } from "react";
 import { GET_BOOKS } from "../../apollo/templates/book/bookQueries";
 import { Loader } from "../../common/components/loader/Loader";
-import { BooksData } from "../../models/book";
 import { BookList } from "./BookList";
 import classes from "./style/bookStyle.module.css";
 
 export const BookScreen: FC = () => {
-  const {
-    data: { books } = {},
-    loading,
-    error,
-  } = useQuery<BooksData>(GET_BOOKS);
+  const { data: { books } = {}, loading, error } = useQuery(GET_BOOKS);
 
-  if (error || !books) return <p>Something went wrong</p>;
+  if (error) return <p>Something went wrong</p>;
 
   return (
     <>
